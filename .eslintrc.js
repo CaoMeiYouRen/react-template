e43cfd29f493ea743f047e0bcebe729916b69428
CaoMeiYouRen <996881204@qq.com>
2020-12-20 21:38:41 +0800

    chore: 修复依赖缺失

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ const IS_PROD = process.env.NODE_ENV === 'production'
 module.exports = {
     root: true,
     extends: [
-        "react-app",
-        "react-app/jest",
+        // "react-app",
+        // "react-app/jest",
         'cmyr'
     ],
     rules: {
