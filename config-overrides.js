a458c5215a7df20bccee9dce407486bf309d753b
CaoMeiYouRen <996881204@qq.com>
2021-02-02 23:06:27 +0800

    feat: 新增 less 支持

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,39 @@ const path = require('path')
 const paths = require('react-scripts/config/paths')
 module.exports = function override(config, env) {
     // 关闭sourceMap
-    config.devtool = false
+    // config.devtool = false
     // 配置打包后的文件位置
     config.output.path = path.join(__dirname, '/dist')
     config.output.publicPath = '/'
     // 配置favicon公共文件打包目录
     paths.appBuild = path.join(path.dirname(paths.appBuild), 'dist')
+
+    const loaders = config.module.rules.find((rule) => Array.isArray(rule.oneOf)).oneOf
+
+    // Insert less-loader as the penultimate item of loaders (before file-loader)
+    loaders.splice(
+        loaders.length - 1,
+        0,
+        {
+            test: /\.less$/i,
+            use: [
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                },
+                {
+                    loader: 'less-loader',
+                    options: {
+                        lessOptions: {
+                            strictMath: true,
+                        },
+                    },
+                },
+            ],
+        },
+    )
+
     return config
 }
